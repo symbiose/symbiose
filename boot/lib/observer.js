a15ed000c5df1591b2109254f37fa7e0d19a4148
Simon Ser <simon@kegtux.org>
2012-05-09 15:47:52 +0200

    Ajout d'une methode "Webos.Observable.build" pour creer des objets statiques observables.

--- boot/lib/observer.js
+++ boot/lib/observer.js
@@ -1,15 +1,15 @@
 Webos.Observable = function WObservable() {
-	this.observers = [];
+	this._observers = [];
 };
 Webos.Observable.prototype = {
 	bind: function(event, fn) {
-		this.observers.push({
+		this._observers.push({
 			fn: fn,
 			event: event
 		});
 	},
 	unbind: function(event, fn) {
-		this.observers = this.observers.filter(function(el) {
+		this._observers = this._observers.filter(function(el) {
 			if (el.fn !== fn || el.event !== event) {
 				return el;
 			}
@@ -18,10 +18,37 @@ Webos.Observable.prototype = {
 	notify: function(event, data, thisObj) {
 		data = data || {};
 		var scope = thisObj || window;
-		this.observers.forEach(function(el) {
+		this._observers.forEach(function(el) {
 			if (el.event === event) {
 				el.fn.call(scope, data);
 			}
 		});
 	}
+};
+
+Webos.Observable.build = function(object) {
+	object._observers = [];
+	object.bind = function(event, fn) {
+		object._observers.push({
+			fn: fn,
+			event: event
+		});
+	};
+	object.unbind = function(event, fn) {
+		object._observers = this._observers.filter(function(el) {
+			if (el.fn !== fn || el.event !== event) {
+				return el;
+			}
+		});
+	};
+	object.notify = function(event, data, thisObj) {
+		data = data || {};
+		var scope = thisObj || window;
+		object._observers.forEach(function(el) {
+			if (el.event === event) {
+				el.fn.call(scope, data);
+			}
+		});
+	};
+	return object;
 };
