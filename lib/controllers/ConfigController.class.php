c598ec71016ba113e1be386b4c216fc93dafdf3d
Simon Ser <simon@kegtux.org>
2012-05-30 14:45:10 +0200

    Ajout d'une classe pour gerer la configuration.

---
+++ lib/controllers/ConfigController.class.php
@@ -0,0 +1,104 @@
+<?php
+namespace lib\controllers;
+use lib\models\Config;
+use \InvalidArgumentException;
+
+/**
+ * ConfigController permet de gerer les fichiers de configuration.
+ * @author $imon
+ * @version 1.0
+ */
+class ConfigController extends \lib\ServerCallComponent {
+	/**
+	 * Recuperer un parametre de configuration.
+	 * @param string $path Le chemin vers le fichier de configuration.
+	 * @param string $index Le parametre de configuration a recuperer.
+	 * @throws InvalidArgumentException
+	 */
+	protected function get($path, $index) {
+		$config = new Config($this->webos);
+		$config->load($path);
+
+		if (!$config->exist($index)) {
+			throw new InvalidArgumentException('Le parametre de configuration "'.$index.'" est introuvable dans "'.$path.'"');
+		}
+
+		return array('value' => $config->get($index));
+	}
+
+	/**
+	 * Recuperer tous les parametres d'une configuration.
+	 * @param string $path Le chemin vers le fichier de configuration.
+	 */
+	protected function getConfig($path) {
+		$config = new Config($this->webos);
+		$config->load($path);
+
+		return $config->getConfig();
+	}
+
+	/**
+	 * Recuperer tous les parametres d'une configuration utilisateur.
+	 * @param string $path Le chemin vers le fichier de configuration utilisateur.
+	 * @param string $base Le chemin vers le fichier servant de modele si le fichier de configuration utilisateur n'existe pas.
+	 */
+	protected function getUserConfig($path, $base) {
+		$config = new \lib\models\Config($this->webos);
+
+		if (!$this->webos->getUser()->isConnected()) {
+			$config->load($base);
+		} else {
+			$authorisations = $this->webos->getAuthorization();
+			$readAuthorisation = $authorisations->getArgumentAuthorizations($path, 'file', 'read');
+			$writeAuthorisation = $authorisations->getArgumentAuthorizations($path, 'file', 'write');
+
+			if ($authorisations->can($writeAuthorisation)) {
+				if (!$this->webos->managers()->get('File')->exists($path)) {
+					$default = $this->webos->managers()->get('File')->get($base);
+					$default->copy($path);
+				}
+
+				if ($authorisations->can($readAuthorisation)) {
+					$config->load($path);
+				} else {
+					$config->load($base);
+				}
+			} else {
+				$config->load($base);
+			}
+		}
+
+		return $config->getConfig();
+	}
+
+	/**
+	 * Definir un parametre de configuration.
+	 * @param string $path Le chemin vers le fichier de configuration.
+	 * @param string $index Le parametre de configuration a modifier.
+	 * @param string $value La nouvelle valeur du parametre.
+	 */
+	protected function set($path, $index, $value) {
+		$config = new Config($this->webos);
+		$config->load($path);
+
+		$config->set($index, $value);
+
+		$config->save();
+	}
+
+	/**
+	 * Definir plusieurs parametres d'une configuration.
+	 * @param string $path Le chemin vers le fichier de configuration.
+	 * @param array $data Un tableau contenant les nouveaux parametres.
+	 */
+	protected function setConfig($path, $data) {
+		$config = new Config($this->webos);
+		$config->load($path);
+
+		foreach ($data as $index => $value) {
+			$config->set($index, $value);
+		}
+
+		$config->save();
+	}
+}
---
+++ usr/lib/webos/config.js
@@ -0,0 +1,101 @@
+new Webos.ScriptFile('usr/lib/webos/file.js');
+
+Webos.ConfigFile = function WConfigFile(data, file) {
+	Webos.Model.call(this, data);
+	
+	this._file = file;
+};
+Webos.ConfigFile.prototype = {
+	sync: function(callback) {
+		callback = Webos.Callback.toCallback(callback);
+		
+		var that = this;
+		
+		var data = {};
+		var nbrChanges = 0;
+		for (var key in this._unsynced) {
+			if (this._unsynced[key].state === 1) {
+				this._unsynced[key].state = 2;
+				data[key] = this._unsynced[key].value;
+				nbrChanges++;
+			}
+		}
+		
+		if (nbrChanges === 0) {
+			callback.success(this);
+			return;
+		}
+		
+		new Webos.ServerCall({
+			'class': 'ConfigController',
+			method: 'setConfig',
+			arguments: {
+				path: this._file.get('path'),
+				data: data
+			}
+		}).load(new Webos.Callback(function() {
+			for (var key in that._unsynced) {
+				if (that._unsynced[key].state === 2) {
+					that._data[key] = that._unsynced[key].value;
+					delete that._unsynced[key];
+					that.notify('update', { key: key, value: that._data[key].value });
+				}
+			}
+			callback.success(that);
+		}, callback.error));
+	}
+};
+Webos.inherit(Webos.ConfigFile, Webos.Model);
+
+Webos.ConfigFile._cache = {};
+Webos.ConfigFile.get = function(path, data) {
+	var file = Webos.File.get(path);
+	
+	if (Webos.ConfigFile._cache[file.get('path')]) {
+		return Webos.ConfigFile._cache[file.get('path')];
+	} else {
+		return new Webos.ConfigFile($.extend({}, data), file);
+	}
+};
+Webos.ConfigFile.load = function(path, callback) {
+	var file = Webos.File.get(path);
+	callback = Webos.Callback.toCallback(callback);
+	
+	if (Webos.ConfigFile._cache[file.get('path')]) {
+		callback.success(Webos.ConfigFile._cache[file.get('path')]);
+	} else {
+		new Webos.ServerCall({
+			'class': 'ConfigController',
+			method: 'getConfig',
+			arguments: {
+				path: file.get('path')
+			}
+		}).load(new Webos.Callback(function(response) {
+			var config = new Webos.ConfigFile(response.getData(), file);
+			Webos.ConfigFile._cache[file.get('path')] = config;
+			callback.success(config);
+		}, callback.error));
+	}
+};
+Webos.ConfigFile.loadUserConfig = function(path, basePath, callback) {
+	var file = Webos.File.get(path);
+	var baseFile = Webos.File.get(basePath);
+	callback = Webos.Callback.toCallback(callback);
+	
+	if (Webos.ConfigFile._cache[file.get('path')]) {
+		callback.success(Webos.ConfigFile._cache[file.get('path')]);
+	} else {
+		new Webos.ServerCall({
+			'class': 'ConfigController',
+			method: 'getUserConfig',
+			arguments: {
+				path: file.get('path'),
+				base: baseFile.get('path')
+			}
+		}).load(new Webos.Callback(function(response) {
+			var config = new Webos.ConfigFile(response.getData(), file);
+			Webos.ConfigFile._cache[file.get('path')] = config;
+			callback.success(config);
+		}, callback.error));
+	}
+};
