93699898c1766c4e21c475d1809a3a0a6095e0fa
Simon Ser <simon@kegtux.org>
2012-06-23 10:48:22 +0200

    Resolution de l'issue #27 et passage de Eye Of Symbiose a la version 1.3.

--- usr/lib/eos/eos.js
+++ usr/lib/eos/eos.js
@@ -1,6 +1,6 @@
 /**
  * Visionneuse d'images Eye Of Symbiose.
- * @version 1.2
+ * @version 1.3
  * @author $imon
  */
 
@@ -20,7 +20,7 @@ function EyeOfSymbiose(image) {
 	
 	var that = this;
 	
-	this.openImage = function(image, userCallback) {
+	this.open = function(image, userCallback) {
 		userCallback = W.Callback.toCallback(userCallback);
 		
 		image = W.File.get(image);
@@ -36,20 +36,38 @@ function EyeOfSymbiose(image) {
 		this.loadingImage = new W.LoadImage({
 			images: image.getAttribute('realpath'),
 			callback: function(data) {
-				that.window.window('loading', false);
 				delete that.loadingImage;
 				if (data.IsEnd) {
 					that.image.attr('src', image.getAttribute('realpath'));
+					that.window.window('loading', false);
 					that.windowsSize();
 					userCallback.success(that);
 				} else {
+					that.window.window('loading', false);
 					userCallback.error(data);
 				}
 			}
 		});
 	};
 	
-	this.previousImage = function() {
+	this.imageTo = function(position) {
+		if (typeof this.file == 'undefined') {
+			return;
+		}
+		
+		this._getImagesInDir(new W.Callback(function(images) {
+			if (position == -1) {
+				position = images.length - 1;
+			}
+			
+			if (!images[position]) {
+				return;
+			}
+			
+			that.open(images[position]);
+		}));
+	};
+	this.imageBy = function(position) {
 		if (typeof this.file == 'undefined') {
 			return;
 		}
@@ -60,36 +78,37 @@ function EyeOfSymbiose(image) {
 					break;
 				}
 			}
-			var imageIndex = i - 1;
+			var imageIndex = i + position;
 			if (imageIndex >= images.length) {
-				imageIndex = 0;
+				imageIndex -= images.length;
 			}
 			if (imageIndex < 0) {
-				imageIndex = images.length - 1;
+				imageIndex += images.length;
 			}
-			that.openImage(images[imageIndex]);
+			that.open(images[imageIndex]);
 		}));
 	};
 	
+	this.previousImage = function() {
+		this.imageBy(-1);
+	};
 	this.nextImage = function() {
+		this.imageBy(1);
+	};
+	this.firstImage = function() {
+		this.imageTo(0);
+	};
+	this.lastImage = function() {
+		this.imageTo(-1);
+	};
+	this.randomImage = function() {
 		if (typeof this.file == 'undefined') {
 			return;
 		}
 		
 		this._getImagesInDir(new W.Callback(function(images) {
-			for (var i = 0; i < images.length; i++) {
-				if (images[i].getAttribute('path') == that.file.getAttribute('path')) {
-					break;
-				}
-			}
-			var imageIndex = i + 1;
-			if (imageIndex >= images.length) {
-				imageIndex = 0;
-			}
-			if (imageIndex < 0) {
-				imageIndex = images.length - 1;
-			}
-			that.openImage(images[imageIndex]);
+			var pos = Math.round((images.length - 1) * Math.random());
+			that.imageTo(pos);
 		}));
 	};
 	
@@ -180,7 +199,103 @@ function EyeOfSymbiose(image) {
 		});
 	};
 	
+	this.openAboutWindow = function() {
+		$.w.window.about({
+			name: 'Visionneuse d\'images',
+			version: '1.3',
+			description: 'La visionneuse d\'images de GNOME.',
+			author: '$imon',
+			icon: new W.Icon('applications/eos')
+		}).window('open');
+	};
+	
 	var headers = this.window.window('header');
+	
+	var menu = $.w.menuWindowHeader().appendTo(headers);
+	this._menus = {};
+	
+	var imageItem = $.w.menuItem('Fichier').appendTo(menu);
+	imageItemContent = imageItem.menuItem('content');
+	
+	$.w.menuItem('Ouvrir...')
+		.click(function() {
+			new NautilusFileSelectorWindow({
+				parentWindow: that.window
+			}, function(file) {
+				if (typeof file != 'undefined') {
+					that.open(file);
+				}
+			});
+		})
+		.appendTo(imageItemContent);
+	
+	$.w.menuItem('Fermer')
+		.click(function() {
+			that.window.window('close');
+		})
+		.appendTo(imageItemContent);
+	
+	var viewItem = $.w.menuItem('Fichier').appendTo(menu);
+	viewItemContent = viewItem.menuItem('content');
+	
+	$.w.menuItem('Zoom avant')
+		.click(function() {
+			that.zoomIn();
+		})
+		.appendTo(viewItemContent);
+	$.w.menuItem('Zoom arri&egrave;re')
+		.click(function() {
+			that.zoomOut();
+		})
+		.appendTo(viewItemContent);
+	$.w.menuItem('Taille normale', true)
+		.click(function() {
+			that.realSize();
+		})
+		.appendTo(viewItemContent);
+	$.w.menuItem('Taille id&eacute;ale')
+		.click(function() {
+			that.windowsSize();
+		})
+		.appendTo(viewItemContent);
+	
+	var gotoItem = $.w.menuItem('Aller &agrave;').appendTo(menu);
+	gotoItemContent = gotoItem.menuItem('content');
+	
+	$.w.menuItem('Image pr&eacute;c&eacute;dente')
+		.click(function() {
+			that.previousImage();
+		})
+		.appendTo(gotoItemContent);
+	$.w.menuItem('Image suivante')
+		.click(function() {
+			that.nextImage();
+		})
+		.appendTo(gotoItemContent);
+	$.w.menuItem('Premi&egrave;re image', true)
+		.click(function() {
+			that.firstImage();
+		})
+		.appendTo(gotoItemContent);
+	$.w.menuItem('Derni&egrave;re image')
+		.click(function() {
+			that.lastImage();
+		})
+		.appendTo(gotoItemContent);
+	$.w.menuItem('Image al&eacute;atoire', true)
+		.click(function() {
+			that.randomImage();
+		})
+		.appendTo(gotoItemContent);
+	
+	var helpItem = $.w.menuItem('Aide').appendTo(menu);
+	helpItemContent = helpItem.menuItem('content');
+	
+	$.w.menuItem('&Agrave; propos')
+		.click(function() {
+			that.openAboutWindow();
+		})
+		.appendTo(helpItemContent);
 	
 	var toolbar = $.w.toolbarWindowHeader().appendTo(headers);
 	
@@ -233,7 +348,7 @@ function EyeOfSymbiose(image) {
 			if (typeof ui.draggable.data('file') == 'undefined') {
 				return;
 			}
-			that.openImage(ui.draggable.data('file')());
+			that.open(ui.draggable.data('file')());
 			return false;
 		}
 	});
@@ -243,7 +358,7 @@ function EyeOfSymbiose(image) {
 	this.window.window('open');
 	
 	if (typeof image != 'undefined') {
-		this.openImage(image);
+		this.open(image);
 	}
 }
 
