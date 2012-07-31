3ec53b9569d3d457ed172d36462f8435fdeb31c9
Simon Ser <simon@kegtux.org>
2012-07-31 09:42:19 +0200

    Ajout du support des commentaires et gestion des fichiers de traduction corrompus.

--- lib/models/TranslationManager_files.class.php
+++ lib/models/TranslationManager_files.class.php
@@ -14,7 +14,21 @@ class TranslationManager_files extends TranslationManager {
 		
 		$lines = explode("\n", $contents);
 		foreach($lines as $line) {
+			$line = trim($line);
+			
+			if (substr($line, 0, 1) == ';') { //C'est un commentaire
+				continue;
+			}
+			
 			$array = explode('=', $line, 2);
+			
+			if (count($array) != 2) {
+				continue;
+			}
+			if (empty($array[0]) || empty($array[1])) {
+				continue;
+			}
+			
 			$data[$array[0]] = $array[1];
 		}
 		
