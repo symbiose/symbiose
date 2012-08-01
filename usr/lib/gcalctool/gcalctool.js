ce175ed30fe1050c1f97c294ad1c93d5f55b44bd
Simon Ser <simon@kegtux.org>
2012-08-01 16:16:28 +0200

    Traduction de la calculette en fr_FR.

--- usr/lib/gcalctool/gcalctool.js
+++ usr/lib/gcalctool/gcalctool.js
@@ -69,7 +69,7 @@ function GCalcTool() {
 
 				result = eval(calcul);
 			} catch(error){ // si ca ne marche pas on affiche un message d'erreur
-				result = originalCalcul+'\nExpression incorrecte';
+				result = originalCalcul + "\n" + this.translations.get('Incorrect expression');
 			} finally {
 				this.lastResult = result;
 				this.insert(result, true, true);
@@ -84,216 +84,220 @@ function GCalcTool() {
 	
 	var gcalctool = this;
 	
-	this.window = $.w.window({
-		title: 'Calculatrice',
-		icon: new W.Icon('apps/calculator'),
-		width: 370,
-		height: 197,
-		stylesheet: 'usr/share/css/gcalctool/main.css',
-		resizable: false
-	});
+	Webos.Translation.load(function(t) {
+		gcalctool.translations = t;
+		
+		gcalctool.window = $.w.window({
+			title: t.get('Calculator'),
+			icon: new W.Icon('apps/calculator'),
+			width: 370,
+			height: 197,
+			stylesheet: 'usr/share/css/gcalctool/main.css',
+			resizable: false
+		});
+		
+		gcalctool.container = $.w.container().addClass('gcalctool');
+		
+		gcalctool.textarea = $.w.textAreaEntry();
+		gcalctool.textarea.bind('keypress', function(event) {
+			var touche_press = (!document.all) ? event.which : event.keyCode;
 	
-	this.container = $.w.container().addClass('gcalctool');
-	
-	this.textarea = $.w.textAreaEntry();
-	this.textarea.bind('keypress', function(event) {
-		var touche_press = (!document.all) ? event.which : event.keyCode;
-
-		if(touche_press == 13) {
+			if(touche_press == 13) {
+				gcalctool.execute();
+				event.preventDefault(); // Permet d'annuler la touche enter
+			} else {
+				gcalctool.toInsertMode(); // Permet de repasser en mode calcul si on est en mode resultat
+			}
+		});
+		gcalctool.container.append(gcalctool.textarea);
+		
+		// 7, 8, 9, ÷, "Annuler", "Effacer"
+		var container = $.w.container();
+		
+		var button = $.w.button('7');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('8');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('9');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('÷');
+		button.addClass('pink');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button(t.get('Cancel'));
+		button.addClass('gray');
+		button.bind('click', function() {
+			gcalctool.insert('', true);
+		});
+		container.append(button);
+		
+		var button = $.w.button(t.get('Delete'));
+		button.addClass('gray');
+		button.bind('click', function() {
+			var nbrCar = gcalctool.textarea.textAreaEntry('value').length - 1;
+			gcalctool.insert(gcalctool.textarea.textAreaEntry('value').substring(0, nbrCar), true);
+		});
+		container.append(button);
+		
+		gcalctool.container.append(container);
+		
+		// 4, 5, 6, x, (, )
+		
+		var container = $.w.container();
+		
+		var button = $.w.button('4');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('5');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('6');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('×');
+		button.addClass('pink');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('(');
+		button.addClass('gray');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button(')');
+		button.addClass('gray');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		gcalctool.container.append(container);
+		
+		// 1, 2, 3, -, x², √
+		
+		var container = $.w.container();
+		
+		var button = $.w.button('1');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('2');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('3');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('-');
+		button.addClass('pink');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('x²');
+		button.addClass('blue');
+		button.bind('click', function() {
+			gcalctool.insert('²');
+		});
+		container.append(button);
+		
+		var button = $.w.button('√');
+		button.addClass('blue');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		gcalctool.container.append(container);
+		
+		// 0, ".", "="
+		
+		var container = $.w.container();
+		
+		var button = $.w.button('0');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('.');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('%');
+		button.addClass('violet');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('+');
+		button.addClass('pink');
+		button.bind('click', function() {
+			gcalctool.insert($(this).html());
+		});
+		container.append(button);
+		
+		var button = $.w.button('=');
+		button.addClass('enter');
+		button.bind('click', function() {
 			gcalctool.execute();
-			event.preventDefault(); // Permet d'annuler la touche enter
-		} else {
-			gcalctool.toInsertMode(); // Permet de repasser en mode calcul si on est en mode resultat
-		}
-	});
-	this.container.append(this.textarea);
-	
-	// 7, 8, 9, ÷, "Annuler", "Effacer"
-	var container = $.w.container();
-	
-	var button = $.w.button('7');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('8');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('9');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('÷');
-	button.addClass('pink');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('Ann.');
-	button.addClass('gray');
-	button.bind('click', function() {
-		gcalctool.insert('', true);
-	});
-	container.append(button);
-	
-	var button = $.w.button('Eff.');
-	button.addClass('gray');
-	button.bind('click', function() {
-		var nbrCar = gcalctool.textarea.textAreaEntry('value').length - 1;
-		gcalctool.insert(gcalctool.textarea.textAreaEntry('value').substring(0, nbrCar), true);
-	});
-	container.append(button);
-	
-	this.container.append(container);
-	
-	// 4, 5, 6, x, (, )
-	
-	var container = $.w.container();
-	
-	var button = $.w.button('4');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('5');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('6');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('×');
-	button.addClass('pink');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('(');
-	button.addClass('gray');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button(')');
-	button.addClass('gray');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	this.container.append(container);
-	
-	// 1, 2, 3, -, x², √
-	
-	var container = $.w.container();
-	
-	var button = $.w.button('1');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('2');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('3');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('-');
-	button.addClass('pink');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('x²');
-	button.addClass('blue');
-	button.bind('click', function() {
-		gcalctool.insert('²');
-	});
-	container.append(button);
-	
-	var button = $.w.button('√');
-	button.addClass('blue');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	this.container.append(container);
-	
-	// 0, ".", "="
-	
-	var container = $.w.container();
-	
-	var button = $.w.button('0');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('.');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('%');
-	button.addClass('violet');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('+');
-	button.addClass('pink');
-	button.bind('click', function() {
-		gcalctool.insert($(this).html());
-	});
-	container.append(button);
-	
-	var button = $.w.button('=');
-	button.addClass('enter');
-	button.bind('click', function() {
-		gcalctool.execute();
-	});
-	container.append(button);
-	
-	this.container.append(container);
-	
-	this.window.window('content').append(this.container);
-	
-	this.window.window('open');
+		});
+		container.append(button);
+		
+		gcalctool.container.append(container);
+		
+		gcalctool.window.window('content').append(gcalctool.container);
+		
+		gcalctool.window.window('open');
+	}, 'gcalctool');
 }
