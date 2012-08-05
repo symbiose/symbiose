ac88df62b8c8d92fdb36d61f1ceb952c589c306e
Simon Ser <simon@kegtux.org>
2012-08-05 13:54:22 +0200

    Traduction de gnome-login et gnome-register.

--- usr/bin/gnome-login.js
+++ usr/bin/gnome-login.js
@@ -1,70 +1,80 @@
-//On initialise la fenetre de deconnexion
+//On initialise la fenetre de connexion
 var loginWindow = $.w.window({
-	icon: new W.Icon('actions/keys'),
-	title: 'Se connecter',
+	icon: 'actions/keys',
+	title: 'Login',
 	width: 350,
 	resizable: false
 });
 
-var loginWindowContents = loginWindow.window('content');
-
-//Contenu de la fenetre
-$('<img />').attr('src', new W.Icon('actions/keys'))
-	.css('float', 'left')
-	.appendTo(loginWindowContents);
-
-loginWindowContents.append('Veuillez entrer votre nom d\'utilisateur et votre mot de passe :');
-var form = $.w.entryContainer()
-	.appendTo(loginWindowContents)
-	.submit(function() {
-		loginWindow.window('loading', true);
-		var callback = new W.Callback(function() {
-			loginWindow.window('close');
-			var ui = uiSelector.selectButton('value');
-			if (ui == '') {
-				ui = undefined;
-			}
-			W.UserInterface.load(ui);
-		}, function(response) {
-			loginWindow.window('loading', false);
-			response.triggerError('La connexion a &eacute;chou&eacute;');
-			password.passwordEntry('content').val('');
-			username.textEntry('content').focus();
-		});
-		W.User.login(username.textEntry('content').val(), password.passwordEntry('content').val(), callback);
-	});
-var username = $.w.textEntry('Nom d\'utilisateur : ').appendTo(form);
-var password = $.w.passwordEntry('Mot de passe : ').appendTo(form);
-var spoiler = $.w.spoiler('Options').one('spoilershow', function() {
-	W.UserInterface.getList(new W.Callback(function(data) {
-		var uis = {};
-		var defaultUI = undefined;
-		for (var index in data) {
-			(function(ui) {
-				if (ui.type == 'ui') { //Si c'est une interface de travail, pas une interface de connexion
-					uis[ui.name] = (ui.attributes.displayname) ? ui.attributes.displayname : ui.name;
-					if ((ui['default'] && typeof defaultUI != 'undefined') || ui.name == W.UserInterface.current.name()) {
-						defaultUI = ui.name;
-					}
-				}
-			})(data[index]);
-		}
-		uiSelector.selectButton('option', 'choices', uis).selectButton('value', defaultUI);
-	}, function() {}));
-}).appendTo(form);
-var uis = {};
-uis[W.UserInterface.current.name()] = 'Interface actuelle';
-uis[''] = 'Interface par d&eacute;faut';
-var uiSelector = $.w.selectButton('Interface : ', uis).selectButton('value', W.UserInterface.current.name()).appendTo(spoiler.spoiler('content'));
-var buttonContainer = $.w.buttonContainer().appendTo(form);
-$.w.button('Annuler')
-	.appendTo(buttonContainer)
-	.bind('click', function() {
-		loginWindow.window('close');
-	});
-$.w.button('Se connecter', true).appendTo(buttonContainer);
-
 //On ouvre la fenetre
 loginWindow.window('open');
 
-username.textEntry('content').focus();
+loginWindow.window('loading', true);
+
+Webos.Translation.load(function(t) {
+	loginWindow.window('loading', false);
+	
+	loginWindow.window('option', 'title', t.get('Login'));
+	
+	var loginWindowContents = loginWindow.window('content');
+
+	//Contenu de la fenetre
+	$('<img />').attr('src', new W.Icon('actions/keys'))
+		.css('float', 'left')
+		.appendTo(loginWindowContents);
+	
+	loginWindowContents.append(t.get('Please enter your username and password :'));
+	var form = $.w.entryContainer()
+		.appendTo(loginWindowContents)
+		.submit(function() {
+			loginWindow.window('loading', true, {
+				message: t.get('Connecting...')
+			});
+			var callback = new W.Callback(function() {
+				loginWindow.window('close');
+				var ui = uiSelector.selectButton('value');
+				if (ui == '') {
+					ui = undefined;
+				}
+				W.UserInterface.load(ui);
+			}, function(response) {
+				loginWindow.window('loading', false);
+				response.triggerError(t.get('The connection failed'));
+				password.passwordEntry('content').val('');
+				username.textEntry('content').focus();
+			});
+			W.User.login(username.textEntry('content').val(), password.passwordEntry('content').val(), callback);
+		});
+	var username = $.w.textEntry(t.get('Username :')).appendTo(form);
+	var password = $.w.passwordEntry(t.get('Password :')).appendTo(form);
+	var spoiler = $.w.spoiler(t.get('Options')).one('spoilershow', function() {
+		W.UserInterface.getList(new W.Callback(function(data) {
+			var uis = {};
+			var defaultUI = undefined;
+			for (var index in data) {
+				(function(ui) {
+					if (ui.type == 'ui') { //Si c'est une interface de travail, pas une interface de connexion
+						uis[ui.name] = (ui.attributes.displayname) ? ui.attributes.displayname : ui.name;
+						if ((ui['default'] && typeof defaultUI != 'undefined') || ui.name == W.UserInterface.current.name()) {
+							defaultUI = ui.name;
+						}
+					}
+				})(data[index]);
+			}
+			uiSelector.selectButton('option', 'choices', uis).selectButton('value', defaultUI);
+		}, function() {}));
+	}).appendTo(form);
+	var uis = {};
+	uis[W.UserInterface.current.name()] = t.get('Current interface');
+	uis[''] = t.get('Default interface');
+	var uiSelector = $.w.selectButton(t.get('Interface :'), uis).selectButton('value', W.UserInterface.current.name()).appendTo(spoiler.spoiler('content'));
+	var buttonContainer = $.w.buttonContainer().appendTo(form);
+	$.w.button(t.get('Cancel'))
+		.appendTo(buttonContainer)
+		.bind('click', function() {
+			loginWindow.window('close');
+		});
+	$.w.button(t.get('Login'), true).appendTo(buttonContainer);
+	
+	username.textEntry('content').focus();
+}, 'gnome');
--- usr/bin/gnome-register.js
+++ usr/bin/gnome-register.js
@@ -1,126 +1,129 @@
 //On initialise la fenetre
 var registerWindow = $.w.window.dialog({
 	icon: new W.Icon('stock/person'),
-	title: 'S\'inscrire',
+	title: 'Register',
 	width: 450,
 	resizable: false
 });
 
-var registerWindowContents = registerWindow.window('content');
-
-var checkFormFn = function() {
-	var valid = true;
-	for (var inputName in inputs) {
-		(function($input) {
-			if ($.webos.widget.is($input, 'textEntry')) {
-				valid = $input.textEntry('isValid');
-			}
-			if ($.webos.widget.is($input, 'passwordEntry')) {
-				valid = $input.passwordEntry('isValid');
-			}
-			if ($.webos.widget.is($input, 'captchaEntry')) {
-				valid = $input.captchaEntry('isValid');
-			}
-		})(inputs[inputName]);
-		
-		if (!valid) {
-			return false;
-		}
-	}
-	return true;
-};
-
-//Contenu de la fenetre
-registerWindowContents.append('Avec un compte, vous pourrez envoyer vos documents, ajuster vos pr&eacute;f&eacute;rences et profiter pleinement des possibilit&eacute;s du webos. Il vous suffit de remplir ce formulaire :');
-
-var checkInProgress = false;
-var form = $.w.entryContainer()
-	.appendTo(registerWindowContents)
-	.submit(function() {
-		if (!checkFormFn()) {
-			return;
-		}
-		
-		registerWindow.window('loading', true);
-		W.User.register({
-			username: inputs.username.textEntry('value'),
-			realname: inputs.realname.textEntry('value'),
-			password: inputs.password.passwordEntry('value'),
-			email: inputs.email.textEntry('value')
-		}, {
-			id: inputs.captcha.captchaEntry('captchaId'),
-			value: inputs.captcha.captchaEntry('value')
-		}, new W.Callback(function() {
-			registerWindow.window('close');
-			$.webos.window.messageDialog({
-				title: 'S\'inscrire',
-				label: 'Votre compte a &eacute;t&eacute; cr&eacute;&eacute; avec succ&egrave;s !',
-				details: 'Vous pouvez vous connecter avec vos identifiants d&egrave;s maintenant.'
-			}).window('open');
-		}, function(response) {
-			registerWindow.window('loading', false);
-			response.triggerError('L\'inscription a &eacute;chou&eacute;');
-		}));
-	})
-	.bind('textentrycheck passwordentrycheck', function(e, data) {
-		if (!data.valid) {
-			registerButton.button('disabled', true);
-		} else {
-			registerButton.button('disabled', !checkFormFn());
-		}
-	});
-var inputs = {};
-inputs.realname = $.w.textEntry('Votre nom : ').keyup(function() {
-	inputs.username.textEntry('value', inputs.realname.textEntry('value').toLowerCase().replace(/[^a-z0-9-_\.]/g, '')).trigger('keyup');
-}).textEntry('option', 'check', function(value) {
-	return (value.length > 0);
-}).appendTo(form);
-inputs.username = $.w.textEntry('Choisissez un nom d\'utilisateur : ').keyup(function() {
-	inputs.username.textEntry('value', inputs.username.textEntry('value').toLowerCase().replace(/[^a-z0-9-_\.]/g, ''));
-}).textEntry('option', 'check', function(value) {
-	return (value.length > 0);
-}).appendTo(form);
-inputs.email = $.w.textEntry('Entrez votre adresse e-mail : ').textEntry('option', 'check', function(value) {
-	return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
-}).appendTo(form);
-inputs.password = $.w.passwordEntry('Choisissez un mot de passe : ').passwordEntry('option', 'check', function(value) {
-	return (Webos.User.evalPasswordPower(value) > 0);
-}).appendTo(form);
-$.w.label('Fiabilit&eacute; du mot de passe :').appendTo(form);
-var passwordPowerIndicator = $.webos.progressbar().appendTo(form);
-inputs.password.keyup(function() {
-	var passwordPower = Webos.User.evalPasswordPower(inputs.password.passwordEntry('value'));
-	passwordPowerIndicator.progressbar('value', passwordPower);
-});
-inputs.passwordCheck = $.w.passwordEntry('Retapez votre mot de passe : ').passwordEntry('option', 'check', function(value) {
-	return (value.length > 0 && inputs.password.passwordEntry('value') == value);
-}).appendTo(form);
-inputs.captcha = $.w.captchaEntry().appendTo(form);
-
-var buttonContainer = $.w.buttonContainer().appendTo(form);
-$.w.button('Annuler')
-	.appendTo(buttonContainer)
-	.bind('click', function() {
-		registerWindow.window('close');
-	});
-var registerButton = $.w.button('M\'inscrire', true).button('disabled', true).appendTo(buttonContainer);
-
 //On ouvre la fenetre
 registerWindow.window('open');
 
-inputs.realname.textEntry('content').focus();
-
 registerWindow.window('loading', true);
-Webos.User.canRegister(function(canRegister) {
-	registerWindow.window('loading', false);
+
+Webos.Translation.load(function(t) {
+	registerWindow.window('option', 'title', t.get('Register'));
 	
-	if (!canRegister) {
-		registerWindow.window('close');
-		$.webos.window.messageDialog({
-			title: 'S\'inscrire',
-			type: 'warning',
-			label: 'L\'inscription a &eacute;t&eacute; d&eacute;sactiv&eacute;e sur ce webos.',
-			details: 'Pour obtenir un compte, veuillez contacter l\'administrateur syst&egrave;me.'
-		}).window('open');
-	}
-});
+	var registerWindowContents = registerWindow.window('content');
+	
+	var checkFormFn = function() {
+		var valid = true;
+		for (var inputName in inputs) {
+			(function($input) {
+				if ($.webos.widget.is($input, 'textEntry')) {
+					valid = $input.textEntry('isValid');
+				}
+				if ($.webos.widget.is($input, 'passwordEntry')) {
+					valid = $input.passwordEntry('isValid');
+				}
+				if ($.webos.widget.is($input, 'captchaEntry')) {
+					valid = $input.captchaEntry('isValid');
+				}
+			})(inputs[inputName]);
+			
+			if (!valid) {
+				return false;
+			}
+		}
+		return true;
+	};
+	
+	//Contenu de la fenetre
+	registerWindowContents.append(t.get('With an account, you can upload your documents, adjust your preferences and enjoy all the features of this webos. Simply fill in this form :'));
+	
+	var checkInProgress = false;
+	var form = $.w.entryContainer()
+		.appendTo(registerWindowContents)
+		.submit(function() {
+			if (!checkFormFn()) {
+				return;
+			}
+			
+			registerWindow.window('loading', true);
+			W.User.register({
+				username: inputs.username.textEntry('value'),
+				realname: inputs.realname.textEntry('value'),
+				password: inputs.password.passwordEntry('value'),
+				email: inputs.email.textEntry('value')
+			}, {
+				id: inputs.captcha.captchaEntry('captchaId'),
+				value: inputs.captcha.captchaEntry('value')
+			}, new W.Callback(function() {
+				registerWindow.window('close');
+				$.webos.window.messageDialog({
+					title: t.get('Register'),
+					label: t.get('Your account has been created !'),
+					details: t.get('You can login from now.')
+				}).window('open');
+			}, function(response) {
+				registerWindow.window('loading', false);
+				response.triggerError(t.get('Registration failed'));
+			}));
+		})
+		.bind('textentrycheck passwordentrycheck', function(e, data) {
+			if (!data.valid) {
+				registerButton.button('disabled', true);
+			} else {
+				registerButton.button('disabled', !checkFormFn());
+			}
+		});
+	var inputs = {};
+	inputs.realname = $.w.textEntry(t.get('Your name :')).keyup(function() {
+		inputs.username.textEntry('value', inputs.realname.textEntry('value').toLowerCase().replace(/[^a-z0-9-_\.]/g, '')).trigger('keyup');
+	}).textEntry('option', 'check', function(value) {
+		return (value.length > 0);
+	}).appendTo(form);
+	inputs.username = $.w.textEntry(t.get('Choose a username :')).keyup(function() {
+		inputs.username.textEntry('value', inputs.username.textEntry('value').toLowerCase().replace(/[^a-z0-9-_\.]/g, ''));
+	}).textEntry('option', 'check', function(value) {
+		return (value.length > 0);
+	}).appendTo(form);
+	inputs.email = $.w.textEntry(t.get('Enter your e-mail adress :')).textEntry('option', 'check', function(value) {
+		return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
+	}).appendTo(form);
+	inputs.password = $.w.passwordEntry(t.get('Choose a password :')).passwordEntry('option', 'check', function(value) {
+		return (Webos.User.evalPasswordPower(value) > 0);
+	}).appendTo(form);
+	$.w.label(t.get('Password strength :')).appendTo(form);
+	var passwordPowerIndicator = $.webos.progressbar().appendTo(form);
+	inputs.password.keyup(function() {
+		var passwordPower = Webos.User.evalPasswordPower(inputs.password.passwordEntry('value'));
+		passwordPowerIndicator.progressbar('value', passwordPower);
+	});
+	inputs.passwordCheck = $.w.passwordEntry(t.get('Retype your password :')).passwordEntry('option', 'check', function(value) {
+		return (value.length > 0 && inputs.password.passwordEntry('value') == value);
+	}).appendTo(form);
+	inputs.captcha = $.w.captchaEntry().appendTo(form);
+	
+	var buttonContainer = $.w.buttonContainer().appendTo(form);
+	$.w.button(t.get('Cancel'))
+		.appendTo(buttonContainer)
+		.bind('click', function() {
+			registerWindow.window('close');
+		});
+	var registerButton = $.w.button(t.get('Register'), true).button('disabled', true).appendTo(buttonContainer);
+	
+	Webos.User.canRegister(function(canRegister) {
+		registerWindow.window('loading', false).window('center');
+		
+		if (!canRegister) {
+			registerWindow.window('close');
+			$.webos.window.messageDialog({
+				title: t.get('Register'),
+				type: 'warning',
+				label: t.get('Registration has been disabled on this webos.'),
+				details: t.get('To get an account, please contact the system administrator.')
+			}).window('open');
+		}
+	});
+}, 'gnome');
