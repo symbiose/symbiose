2d1edcb14495c9c7df70b04ca054cce3b1f2ccc9
spartan-117 <qhm11@hotmail.com>
2012-09-10 12:40:58 +0200

    Amélioration du "bug" 85 Il est désormais possible de passé en paramètre l'option -l ainsi que le chemin à lister

--- usr/bin/ls.php
+++ usr/bin/ls.php
@@ -5,20 +5,12 @@ $authorisations = $this->webos->getAuthorization();
 $requiredAuthorisation = $authorisations->getArgumentAuthorizations($path, 'file', 'read');
 $authorisations->control($requiredAuthorisation);
 
-$files = $this->webos->managers()->get('File')->get($path)->contents();
-
-if ($this->arguments->isOption('r')) {
-	$files = array_reverse($files, true);
-}
-
-$colors = array('blue'=>'729fcf','green'=>'8ae234','white'=>'a9a9a9','magenta'=>'ac7fa7','red'=>'ee2929');
-
-foreach ($files as $file) {
-	if (!$this->arguments->isOption('a') && preg_match('#^\.#', $file->basename()))
-		continue;
-
+/**
+ *
+ */
+function print_file_ls($file, $LOption, $colors) {
 	$color = 'white';
-
+	
 	if ($file->isDir()) {
 		$color = 'blue';
 		$balise = 'strong';
@@ -27,7 +19,7 @@ foreach ($files as $file) {
 		if($ext == 'php' OR $ext == 'js')
 			$color = 'green';
 		elseif($ext == 'jpeg' OR $ext == 'jpg' OR $ext == 'gif' OR $ext == 'png' OR $ext == 'tiff'
-		OR $ext == 'mp3' OR $ext == 'ogg' OR $ext == 'wav')
+			OR $ext == 'mp3' OR $ext == 'ogg' OR $ext == 'wav')
 			$color = 'magenta';
 		elseif($ext == 'tar' OR $ext == 'zip')
 			$color = 'red';
@@ -37,6 +29,45 @@ foreach ($files as $file) {
 		else
 			$balise = 'span';
 	}
+	if ($LOption)
+		echo $file->size().' '.date ('D m y H:i', $file->mtime()).' <'.$balise.' style="color: #'.$colors[$color].';">'.$file->basename().'</'.$balise.'>&emsp;<br />' ;
+	else
+		echo '<'.$balise.' style="color: #'.$colors[$color].';">'.$file->basename().'</'.$balise.'>&emsp; ' ;
+}
 
-	echo '<'.$balise.' style="color: #'.$colors[$color].';">'.$file->basename().'</'.$balise.'>&emsp;';
+
+
+$colors = array('blue'=>'729fcf','green'=>'8ae234','white'=>'a9a9a9','magenta'=>'ac7fa7','red'=>'ee2929');
+
+$Params = $this->arguments->getParams() ;// On récupères les paramètres qui ne sont pas de Options
+if(sizeof($Params) == 0)
+	$Params = array('./') ;
+
+foreach ($Params as $param) {
+	$files = 0 ;// on initialise le tableaux de fichiers
+	$fileName = htmlspecialchars($param, ENT_QUOTES) ;// on récupère le contenu d'un paramètres
+	
+	try {
+		$files = $this->webos->managers()->get('File')->get($path.(($fileName != '') ?'/'.$fileName : '')) ;//On ouvre le dossier ->contents()
+		
+	}
+	catch(Exception $e) {
+		echo '<strong style="color: #'.$colors['red'].';">Le dossier : '.$fileName.' n\'existe pas!</strong><br />' ;
+		continue ;
+	}
+
+	
+	if($files->isDir()) {
+		$files = $files->contents() ;
+		if ($this->arguments->isOption('r'))
+			$files = array_reverse($files, true);
+		foreach ($files as $file) {
+			if (!$this->arguments->isOption('a') && preg_match('#^\.#', $file->basename()))
+				continue;
+			print_file_ls($file, $this->arguments->isOption('l'), $colors) ;
+		}
+	}
+	else
+		print_file_ls($files, $this->arguments->isOption('l'), $colors) ;
+	
 }
