022c2704e7523248e2cfd3415f42de393c9a8ac8
Simon Ser <simon@kegtux.org>
2012-12-16 19:46:29 +0100

    Renamed /var/logs/ -> /var/log/.

--- lib/models/Log.class.php
+++ lib/models/Log.class.php
@@ -11,16 +11,16 @@ abstract class Log extends \lib\WebosComponent {
 	protected $log; //La ligne de log
 
 	/**
-	 * Enregistrer le log
+	 * Enregistrer le log.
 	 * @param string $file Le fichier de journalisation.
 	 */
 	protected function _save($file) {
 		$line = date('M d G:i:s').' '.$_SERVER['SERVER_NAME'].' '.$this->log;
-		@file_put_contents('var/logs/'.$file, $line."\n", FILE_APPEND);
+		@file_put_contents('var/log/'.$file, $line."\n", FILE_APPEND);
 	}
 
 	/**
-	 * Enregistrer le log
+	 * Enregistrer le log.
 	 */
 	abstract public function save();
 }
