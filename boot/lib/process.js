f982ccdda4d36ad31d6e23c98e19bc35d47f0183
$imon <simon@kegtux.org>
2013-04-17 12:32:46 +0300

    Added JSDoc to Webos.Process.

--- boot/lib/process.js
+++ boot/lib/process.js
@@ -1,3 +1,10 @@
+/**
+ * A process (an instance of a computer program that is being executed).
+ * @param {Object} options The process' options.
+ * @constructor
+ * @augments {Webos.Observable}
+ * @since  1.0alpha1
+ */
 Webos.Process = function WProcess(options) {
 	this.pid = options.pid;
 	//var key = options.key;
@@ -16,13 +23,27 @@ Webos.Process = function WProcess(options) {
 	Webos.Observable.call(this);
 };
 Webos.Process.prototype = {
+	/**
+	 * The code of the process.
+	 */
 	main: function $_WProcess_main() {},
+	/**
+	 * Get this process' ID.
+	 * @returns {Number} The PID.
+	 */
 	getPid: function $_WProcess_getPid() {
 		return this.pid;
 	},
+	/**
+	 * Get this process' authorizations.
+	 * @returns {Webos.Authorizations} The authorizations.
+	 */
 	getAuthorizations: function $_WProcess_getAuthorizations() {
 		return this.authorizations;
 	},
+	/**
+	 * Start this process.
+	 */
 	run: function $_WProcess_run() {
 		if (this.state() != 'ready') {
 			return;
@@ -58,6 +79,9 @@ Webos.Process.prototype = {
 			});
 		}
 	},
+	/**
+	 * Stop this process.
+	 */
 	stop: function $_WProcess_stop() {
 		if (this.state() != 'running' && this.state() != 'idle') {
 			return;
@@ -73,11 +97,19 @@ Webos.Process.prototype = {
 		delete Webos.Process.list[this.getPid()];
 		delete this;
 	},
+	/**
+	 * Get this process' state.
+	 * @returns {String} This process' state (e.g. "ready", "running", "idle", "killed").
+	 */
 	state: function $_WProcess_state() {
 		var states = ['ready', 'running', 'idle', 'killed'];
 
 		return states[this._state];
 	},
+	/**
+	 * Check if this process is currently running.
+	 * @returns {Boolean} True if this process is running, false otherwise.
+	 */
 	isRunning: function $_WProcess_isRunning() {
 		return (this._state == 1 || this._state == 2);
 	},
@@ -87,11 +119,33 @@ Webos.Process.prototype = {
 };
 Webos.inherit(Webos.Process, Webos.Observable);
 
+/**
+ * A list of all processes.
+ * @type {Object}
+ * @private
+ */
 Webos.Process.list = {};
+
+/**
+ * The current processes' stack.
+ * @type {Array}
+ * @private
+ */
 Webos.Process.stack = [];
+
+/**
+ * Get a process.
+ * @param   {Number} pid The process' ID.
+ * @returns {Webos.Process} The process.
+ */
 Webos.Process.get = function $_WProcess_get(pid) {
 	return Webos.Process.list[pid];
 };
+
+/**
+ * Get the current process.
+ * @returns {Webos.Process} The current process.
+ */
 Webos.Process.current = function $_WProcess_current() {
 	return Webos.Process.stack[Webos.Process.stack.length - 1];
 };
@@ -99,10 +153,19 @@ Webos.Process.current = function $_WProcess_current() {
 Webos.Observable.build(Webos.Process);
 
 
+/**
+ * A set of authorizations.
+ * @param {Array} [authorizations] The list of authorizations.
+ */
 Webos.Authorizations = function WAuthorizations(authorizations) {
 	this.authorizations = authorizations || [];
 };
 Webos.Authorizations.prototype = {
+	/**
+	 * Check if an authorization is in this set.
+	 * @param   {String} auth The authorization's name.
+	 * @returns {Boolean}     True if the authorization is in this set, false otherwise.
+	 */
 	can: function $_WAuthorizations_can(auth) {
 		for (var i = 0; i < this.authorizations.length; i++) {
 			if (this.authorizations[i] == auth) {
@@ -111,14 +174,26 @@ Webos.Authorizations.prototype = {
 		}
 		return false;
 	},
+	/**
+	 * Get a list of all authorizations in this set.
+	 * @returns {Array} The list of authorizations.
+	 */
 	get: function $_WAuthorizations_get() {
 		return this.authorizations;
 	},
+	/**
+	 * Set authorizations in this set.
+	 * @param {Array} auth The list of new authorizations.
+	 */
 	set: function $_WAuthorizations_set(auth) {
 		if (auth != this.authorizations) {
 			this.authorizations = auth;
 		}
 	},
+	/**
+	 * Add an authorization in this set.
+	 * @param {String} auth The authorization.
+	 */
 	add: function $_WAuthorizations_add(auth) {
 		for (var i = 0; i < this.authorizations.length; i++) {
 			if (this.authorizations[i] == auth) {
@@ -128,6 +203,10 @@ Webos.Authorizations.prototype = {
 		
 		this.authorizations.push(auth);
 	},
+	/**
+	 * Remove an authorization from this list.
+	 * @param  {String} auth The authorization.
+	 */
 	remove: function $_WAuthorizations_remove(auth) {
 		for (var i = 0; i < this.authorizations.length; i++) {
 			if (this.authorizations[i] == auth) {
@@ -135,6 +214,11 @@ Webos.Authorizations.prototype = {
 			}
 		}
 	},
+	/**
+	 * Get/set this set's model.
+	 * @param   {String} [value] The new authorizations model for this set.
+	 * @returns {String|Boolean} The current set's model or false if there was an error.
+	 */
 	model: function $_WAuthorizations_model(value) {
 		var compareArraysFn = function(a, b) {
 			if (a.length != b.length) {
@@ -180,19 +264,32 @@ Webos.Authorizations.prototype = {
 	}
 };
 
-Webos.Authorizations.all = ['file.user.read',
-                       'file.user.write',
-                       'file.home.read',
-                       'file.home.write',
-                       'file.system.read',
-                       'file.system.write',
-                       'user.read',
-                       'user.edit',
-                       'user.manage',
-                       'package.read',
-                       'package.write',
-                       'package.checked.manage',
-                       'package.unchecked.manage'];
+/**
+ * A list of all authorizations.
+ * @type {Array}
+ * @private
+ */
+Webos.Authorizations.all = [
+	'file.user.read',
+	'file.user.write',
+	'file.home.read',
+	'file.home.write',
+	'file.system.read',
+	'file.system.write',
+	'user.read',
+	'user.edit',
+	'user.manage',
+	'package.read',
+	'package.write',
+	'package.checked.manage',
+	'package.unchecked.manage'
+];
+
+/**
+ * The authorization's models.
+ * @type {Object}
+ * @private
+ */
 Webos.Authorizations.models = {
 	'user': ['file.user.read',
              'file.user.write',
