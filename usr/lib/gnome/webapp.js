393437002e64f6f05e62a9856e38335bb97b7669
Simon Ser <simon@kegtux.org>
2013-09-03 11:06:07 +0200

    Improved webapps support. Now checking if the webapp is installed before asking for installation.

--- usr/lib/gnome/webapp.js
+++ usr/lib/gnome/webapp.js
@@ -1,11 +1,17 @@
 (function() {
-	if (!window.navigator.mozApps) {
+	if (!navigator.mozApps) {
 		return;
 	}
 
+	var getManifestPath = function() {
+		var manifestFile = W.File.get('/usr/share/manifests/webos.webapp');
+
+		return window.location.origin+'/'+window.location.pathname+'/'+manifestFile.get('realpath');
+	};
+
 	var installWebapp = function() {
 		var manifestFile = W.File.get('/usr/share/manifests/webos.webapp');
-		var request = window.navigator.mozApps.install(window.location.origin+'/'+window.location.pathname+'/'+manifestFile.get('realpath'));
+		var request = navigator.mozApps.install(getManifestPath());
 
 		/*request.onsuccess = function () {
 			// Save the App object that is returned
@@ -18,16 +24,26 @@
 		};*/
 	};
 
-	var notificationsButtons = [
-		$.w.button('Install app').click(function() {
-			installWebapp();
-		})
-	];
-	
-	$.w.notification({
-		title: 'Install '+Webos.name+' on your device',
-		message: 'You can install the webos to provide a better integration with your device.',
-		icon: new W.Icon('actions/document-save'),
-		widgets: notificationsButtons
-	});
+	var askForInstallation = function() {
+		var notificationsButtons = [
+			$.w.button('Install app').click(function() {
+				installWebapp();
+			})
+		];
+		
+		$.w.notification({
+			title: 'Install '+Webos.name+' on your device',
+			message: 'You can install the webos to provide a better integration with your device.',
+			icon: new W.Icon('actions/document-save'),
+			widgets: notificationsButtons
+		});
+	};
+
+	//Check if the app is already installed
+	var request = navigator.mozApps.checkInstalled(getManifestPath());
+	request.onsuccess = function() {
+		if (!request.result) { //App not installed
+			askForInstallation();
+		}
+	};
 })();
