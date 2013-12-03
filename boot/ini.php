3bdcd8fc7435fd95e580e895be5d5d4eaecfb5cd
Simon Ser <contact@emersion.fr>
2013-12-03 07:49:52 +0100

    Added unparse_url() in PHP.

--- boot/ini.php
+++ boot/ini.php
@@ -57,6 +57,25 @@ if (PHP_VERSION_ID < getPHPVersionId($minPHPVersion)) { //Outdated version
 }
 
 /**
+ * Unparse a parsed URL.
+ * @param  array $parsed_url The parsed URL.
+ * @return string            The unparsed URL.
+ * @see    http://php.net/manual/en/function.parse-url.php#106731
+ */
+function unparse_url($parsed_url) {
+	$scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+	$host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+	$port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+	$user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+	$pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
+	$pass     = ($user || $pass) ? "$pass@" : '';
+	$path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+	$query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+	$fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+	return "$scheme$user$pass$host$port$path$query$fragment";
+} 
+
+/**
  * Log an error.
  */
 function errorLogger($errno, $errstr, $errfile, $errline) {
