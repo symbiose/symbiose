72cdd3182601063a210e8efe6f687754123d956c
Simon Ser <contact@emersion.fr>
2014-02-26 20:49:27 +0100

    Removed unused scripts.

--- bin/halt.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 'Stopping processes...'."\n";
-
-$processManager = $this->managers()->getManagerOf('process');
-$processes = $processManager->listAll();
-foreach($processes as $process) {
-	echo 'Stopping process #'.$process->id().'...'."\n";
-	$processManager->kill($process->id());
-}
