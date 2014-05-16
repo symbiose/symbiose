0ee4bc3e920c647f09958a494f0fa18d614700b3
Simon Ser <contact@emersion.fr>
2014-05-16 22:39:56 +0200

    Adds -e and -n to echo.

--- usr/bin/echo.js
+++ usr/bin/echo.js
@@ -5,6 +5,38 @@ var proc = this;
 
 	var params = args.getParams();
 
-	term.echo(params.join(' '));
+	var output = params.join(' ');
+
+	if (args.isOption('e')) {
+		output = args.getOption('e') + output;
+
+		// See `man echo`
+		var escapedChars = [
+			['\\\\', '\\'],
+			//['\\a', '\a'],
+			['\\b', '\b'],
+			//['\\c', '\c'],
+			//['\\e', '\e'],
+			//['\\f', '\f'],
+			['\\n', '\n'],
+			['\\r', '\r'],
+			['\\t', '\t'],
+			['\\v', '\v']
+		];
+
+		for (var i = 0; i < escapedChars.length; i++) {
+			var escaped = escapedChars[i];
+
+			output = output.replace(new RegExp(escaped[0].replace(/\\/g, '\\\\'), 'g'), escaped[1]);
+		}
+	}
+
+	if (!args.isOption('n')) {
+		output += '\n';
+	} else {
+		output = args.getOption('n') + output;
+	}
+
+	term.echo(output);
 	proc.stop();
 })();
